bfc01b84c95b2114bbfdb4a62f3a991cf3879f5f
GiacomoDiNardo <vest_95@hotmail.it>
2022-11-21 16:52:25 +0100

    Aggiungere i file di progetto.

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,27 @@
+﻿using la_mia_pizzeria_static.Data;
+using la_mia_pizzeria_static.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace la_mia_pizzeria_static.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult Index()
+        {
+            PizzeriaDbContext db = new PizzeriaDbContext();
+            List<Pizza> listaPizze = db.Pizze.ToList();
+
+
+            return View(listaPizze);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            PizzeriaDbContext db = new PizzeriaDbContext();
+
+            Pizza pizza = db.Pizze.Where(p => p.PizzaId == id).FirstOrDefault();
+            return View(pizza);
+        }
+    }
+}
---
+++ Models/Pizza.cs
@@ -0,0 +1,24 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public int PizzaId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public int Prezzo { get; set; }
+
+        public Pizza ()
+        {
+
+        }
+
+        public Pizza (string name, string description, int prezzo, string image)
+        {
+            Name = name;
+            Description = description;
+            Prezzo = prezzo;
+            Image = image;
+        }
+    }
+}
